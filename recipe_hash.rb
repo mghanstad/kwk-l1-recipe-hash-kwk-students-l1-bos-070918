874537500d932a4a6c006b8b3174187cf2d276ef
mghanstad <mghanstad@gmail.com>
2018-07-11 18:54:04 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -16,9 +16,9 @@
 perfect_10_recipe = {}
  , almond flour => "2 cups"
  , gluten_freewhole_oats => "1 cup"
-,   kosher salt - 1/2 tea spoon
-#  - baking powder  - 1/2 tea spoon
-#  - baking soda - 1/4 tea spoon
+ , kosher salt => "1/2 tea spoon"
+ , baking powder => "1/2 tea spoon"
+ , baking soda => "1/4 tea spoon"
 #  - Xanthan Gum - 1/2 tea spoon
 #  - slivered almonds, lightly toasted - 1/4 cup
 #  - mini dark chocolate chips - 3/4 cup
